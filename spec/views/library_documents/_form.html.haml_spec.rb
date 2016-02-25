454451e2bfed0b6878635dcaeb35e7dbe703e9a3
Lydia Chang <mathmerized@gmail.com>
2016-02-25 11:01:52 -0500

    update test for form

--- spec/views/library_documents/_form.html.haml_spec.rb
+++ spec/views/library_documents/_form.html.haml_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "library_documents/_form.html.haml", :type => :view do
   it 'fields' do
     assign(:category, create(:library_category))
-    assign(:document, create(:library_document))
+    assign(:document, LibraryDocument.new)
     render
 
     expect(rendered).to have_field('library_document_name')
