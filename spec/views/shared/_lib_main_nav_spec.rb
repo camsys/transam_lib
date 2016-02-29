5621bc03f313fba904a60332dccd9ae13a605248
Lydia Chang <mathmerized@gmail.com>
2016-02-29 08:54:27 -0500

    restrict lib main nav to categories in org list

--- spec/views/shared/_lib_main_nav_spec.rb
+++ spec/views/shared/_lib_main_nav_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "shared/_lib_main_nav.html.haml", :type => :view do
   it 'links' do
     test_category = create(:library_category)
+    assign(:organization_list, [test_category.organization.id])
     render
 
     expect(rendered).to have_link('Document Library')
