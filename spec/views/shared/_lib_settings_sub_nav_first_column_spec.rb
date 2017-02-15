c207632391a9fdafa8bfbc429d12dab7e3cba67f
Lydia Chang <mathmerized@gmail.com>
2017-02-15 11:59:03 -0500

    setup partials for header menu

--- spec/views/shared/_lib_settings_sub_nav_first_column_spec.rb
+++ spec/views/shared/_lib_settings_sub_nav_first_column_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "shared/_lib_admin_sub_nav.html.haml", :type => :view do
+describe "shared/_lib_settings_sub_nav_first_column.html.haml", :type => :view do
   it 'links' do
     allow(controller).to receive(:current_ability).and_return(Ability.new(create(:admin)))
     render
