0301668e8aa7c1ab7c466f5c2f3a490ba119b544
Alex Andrade <aandrade@camsys.com>
2019-02-20 17:02:49 -0500

    Surround factory values with blocks to get tests passing.

--- spec/factories/library_categories.rb
+++ spec/factories/library_categories.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
 
   factory :library_category do
     association :organization
-    name 'Test Library Category'
-    description 'test library category description'
-    active true
+    name { 'Test Library Category' }
+    description { 'test library category description' }
+    active { true }
   end
 end
--- spec/factories/library_documents.rb
+++ spec/factories/library_documents.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
     association :library_category
     association :organization
     association :creator, factory: :normal_user
-    original_filename 'test_doc.pdf'
-    name 'test doc'
-    description 'Test Document'
+    original_filename { 'test_doc.pdf' }
+    name { 'test doc' }
+    description { 'Test Document' }
     file { Rack::Test::UploadedFile.new(File.join(TransamCore::Engine.root, 'spec', 'support', 'test_files', 'test_doc.pdf')) }
   end
 end
